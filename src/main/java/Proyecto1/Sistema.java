185d07fdd08ea05044eb87db2eca95d163ee9328
JorgeMawyin <jmawyin@espol.edu.ec>
2022-06-21 18:23:42 -0500

    Actualización de la clase Sistema

--- src/main/java/Proyecto1/Sistema.java
+++ src/main/java/Proyecto1/Sistema.java
@@ -13,10 +13,13 @@ public class Sistema {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        /*System.out.println("MENU");
-        System.out.println("1. Crear cita");
-        System.out.println("2. Eliminar cita");
-        System.out.println("3. Consultar cita");
+        System.out.println("MENU");
+        System.out.println("1. Servicio");
+        System.out.println("2. Empleado");
+        System.out.println("3. Clientes");
+        System.out.println("4. Citas");
+        System.out.println("5. Atenciones");
+        System.out.println("6. Salir");
         System.out.println("Ingrese la opción deseada:");
         int opcion = sc.nextInt();
         switch (opcion){
@@ -26,7 +29,33 @@ public class Sistema {
                 break;
             case 3:
                 break;
-        }*/
+            case 4:
+                System.out.println("MENU");
+                System.out.println("1. Crear cita");
+                System.out.println("2. Eliminar cita");
+                System.out.println("3. Consultar cita");
+                System.out.println("Ingrese la opción deseada:");
+                int opcionCita = sc.nextInt();
+                
+                switch(opcionCita){
+                    case 1:
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                    default:
+                        break;
+                }
+                break;
+            case 5:
+                break;
+            case 6:
+                System.out.println("Gracias por preferirnos :D");
+                break;
+            default:
+                break;
+        }
         
     }
 }
